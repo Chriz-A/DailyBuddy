cfe4fc01e30424318b2ca17581a3b52d5cc8998d
Chris Anastasiu <christopher.anastasiu@stud.uni-regensburg.de>
2016-08-23 02:29:11 +0200

    menu commit 2

--- app/src/main/java/de/ur/mi/android/dailybuddy/Kalender.java
+++ app/src/main/java/de/ur/mi/android/dailybuddy/Kalender.java
@@ -1,32 +1,27 @@
 package de.ur.mi.android.dailybuddy;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Chris on 23.08.2016.
+ */
+public class Kalender extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.kalender);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -48,9 +43,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         if (id == R.id.kalenderansicht_abrufen) {
-            Intent i = new Intent(this, Kalender.class);
-            startActivity(i);
-            return true;
+
         }
         if (id == R.id.notizfunktion_aufrufen) {
             return true;
---
+++ app/src/main/java/de/ur/mi/android/dailybuddy/Notizen.java
@@ -0,0 +1,9 @@
+package de.ur.mi.android.dailybuddy;
+
+import android.app.Activity;
+
+/**
+ * Created by Chris on 23.08.2016.
+ */
+public class Notizen extends Activity {
+}
